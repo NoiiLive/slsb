8fa0469b327d53cf2f6ec9313274c33cf886011f
NoiiLive <noiit@boii-underage>
2024-05-11 22:54:32 -0700

    spammer

--- src/main/java/net/clozynoii/slsb/procedures/CRankHealthUpdateProcedure.java
+++ src/main/java/net/clozynoii/slsb/procedures/CRankHealthUpdateProcedure.java
@@ -2,13 +2,14 @@ package net.clozynoii.slsb.procedures;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
 
 public class CRankHealthUpdateProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		((LivingEntity) entity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.MAX_HEALTH).setBaseValue(1500);
-		if (entity instanceof LivingEntity _entity)
-			_entity.setHealth(entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1);
+		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+			_entity.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 99999999, 124, false, false));
 	}
 }
